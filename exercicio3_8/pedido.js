c7e8a9f841c07978f97d41757c3b3de9800a331f
karlacvida <karlacvida@hotmail.com>
2022-08-05 19:51:19 -0300

    exercicio6

--- exercicio3_8/pedido.js
+++ exercicio3_8/pedido.js
@@ -1,17 +1,25 @@
 import { Produto } from "./Produto.js";
 export class Pedido {
-  constructor(numPedido, nomeCliente) {
-    this.numeroPedido = numPedido;
-    this.nomeCliente = nomeCliente;
-    this.dataPedido = new Date().toLocaleDateString();
-    this.estaPago = false;
-    this.listaProdutos = [];
-  }
-}
-
-adicionarProduto(produto) {
-    const ehProduto = produto instanceof Produto;
-    if (ehProduto) {
-      this.listaProdutos.push(produto);
+    constructor(numPedido, nomeCliente) {
+      this.numeroPedido = numPedido;
+      this.nomeCliente = nomeCliente;
+      this.dataPedido = new Date().toLocaleDateString();
+      this.estaPago = false;
+      this.listaProdutos = [];
+    }
+  
+    adicionarProduto(produto) {
+      const ehProduto = produto instanceof Produto;
+      if (ehProduto) {
+        this.listaProdutos.push(produto);
+      }
+    }
+  
+    calcularTotal() {
+      let total = 0;
+      this.listaProdutos.forEach((produto) => {
+        total = total + produto.preco * produto.quantidade;
+      });
+      return total;
     }
   }
